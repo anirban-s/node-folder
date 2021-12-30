b6e9511ff3c58c59a61626af3d1e926320c4e863
anirban-s <baban.chrome@gmail.com>
2021-12-30 18:58:21 +0530

    global error handling added

--- server.js
+++ server.js
@@ -2,11 +2,28 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+// GLOBAL ERROR HANDLER FOR UNCAUGHT EXCEPTIONS
+process.on('uncaughtException', (err) => {
+  console.log('UNEXCEPTION! Shutting down the server....');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
 // STARTING SERVER
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`The server is up on port ${port}`);
+});
+
+// GLOBAL ERROR HANDLER FOR UNHANDLED REJECTIONS
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTED PROMISES! Shutting down the server....');
+  // Shutting the server gracefully on unhandled rejection.
+  server.close(() => {
+    process.exit(1);
+  });
 });
