223dac91cb88ba35b625681ea13047282360405f
anirban-s <baban.chrome@gmail.com>
2021-12-30 17:11:16 +0530

    eslint.json file added with eslint configuration

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
+
 // STARTING SERVER
 app.listen(port, () => {
   console.log(`The server is up on port ${port}`);
